d5b8d6ed18214a086bb2f4cb63ffac2d7f6cbbb3
SanderLentink.nl <git@lent.ink>
2024-11-25 17:22:40 +0100

    DOMPurify

--- mods/markdown.js
+++ mods/markdown.js
@@ -3,27 +3,22 @@ import { LoadElem, substitute_baseURLs } from './abstractelem.js'
 
 import * as markedhack from 'https://cdn.lent.ink/js/npm/marked.js'
 const { parse } = window.npm['marked']
-
+const DOMPurify = window.npm['dompurify'].default
 
 class Markdown extends LoadElem {
     container_classname(){ return 'markdown' }
-    parse_markdown(){
-        let data = this.raw
-        //FIXME this needs some more security checking
-        if ( (! data.includes("<script") && ! data.includes("<iframe") && ! data.includes("eval(") && ! data.includes("<html")) ||
-            window.localStorage.getItem(window.dataurl) ||
-            confirm('This page wants to load markdown which might run JS, okay?'))
-            window.localStorage.setItem(window.dataurl, 'allow_markdown_loading')
-        else
-            return 'Loading Markdown not permitted'
-
+    parse_markdown(data){
         let parsed = 'ERROR failed parsing markdown.' + data
         try {
-            parsed = /*marked.*/parse(data)
+            parsed = parse(data)
         } catch (e) {
             parsed += e
         }
         return parsed
+    }
+    set_data(inp){
+        this.raw = inp
+        this.container.innerHTML = DOMPurify.sanitize(this.parse_markdown(inp))
     }
     constructor(inp){
         super({})
@@ -33,13 +28,11 @@ class Markdown extends LoadElem {
         if (inp.endsWith('.md')){
             this.url = substitute_baseURLs(inp)
             this.loadURL(this.url, (data) => {
-                this.raw = data
-                this.container.innerHTML = this.parse_markdown()
+                this.set_data(data)
             })
         }
         else {
-          this.raw = inp
-          this.container.innerHTML = this.parse_markdown()
+            this.set_data(inp)
         }
     }
 

064e4f9e2db99b7a04db392c137ecfdbdd71fa70
SanderLentink.nl <git@lent.ink>
2024-11-27 21:47:23 +0100

    changed to mjs

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@ set -e
 curl --output package.json https://raw.githubusercontent.com/svlentink/www/refs/heads/master/cdn.lent.ink/js/npm/package.json
 curl --output mods/storage.mjs https://raw.githubusercontent.com/svlentink/www/refs/heads/master/cdn.lent.ink/js/mod/storage.js
 
-for f in main.js mods/*.js; do
+for f in main.mjs mods/*.js; do
 	sed -i 's_\ //WEBPACK__' $f;
 done
 
